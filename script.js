96af6c40652dad09fa4a83090818aede7a44c7ec
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-01 23:37:43 -0300

    comecei coda o JS

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const botaoMenos = document.getElementById('menos')
 const botaoMais = document.getElementById('mais')
 
+const diplayData = document.getElementById('data')
+
+const data = new Date(); // data atual
+const dataFormatada = data.toLocaleString('pt-BR'); // data no formato brasileiro
+console.log(dataFormatada); // exibe a data no console
+
+diplayData.innerText = dataFormatada
