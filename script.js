5324917f0eae6e526be858b6538bc09734ba4007
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-01 23:54:22 -0300

    terminado logica de contagem no salão

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const botaoMenos = document.getElementById('menos')
 const botaoMais = document.getElementById('mais')
+let assistenciaSalaoInput = document.getElementById('assistencia-salao-input')
+let assistenciaSalao = 0
 
+
+// data
 const diplayData = document.getElementById('data')
 
 const data = new Date(); // data atual
@@ -8,3 +12,24 @@ const dataFormatada = data.toLocaleString('pt-BR'); // data no formato brasileir
 console.log(dataFormatada); // exibe a data no console
 
 diplayData.innerText = dataFormatada
+
+function mostrarAssistencia() {
+    assistenciaSalaoInput.innerText = assistenciaSalao
+}
+
+mostrarAssistencia()
+
+function diminuir() {
+    assistenciaSalao = assistenciaSalao - 1
+    console.log(assistenciaSalao)
+    mostrarAssistencia()
+}
+
+function somar() {
+    assistenciaSalao = assistenciaSalao + 1
+    console.log(assistenciaSalao)
+    assistenciaSalaoInput.innerText = assistenciaSalao
+}
+
+botaoMenos.addEventListener('click', diminuir)
+botaoMais.addEventListener('click', somar)
