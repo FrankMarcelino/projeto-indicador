bdfcf96829ebf624fe8bf5c5067442fb981e10d8
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-02 10:43:19 -0300

    iniciei a o codigo da assistencia Zoom

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const botaoMenos = document.getElementById('menos')
 const botaoMais = document.getElementById('mais')
+const botaoConcluir = document.getElementById('concluir')
 let assistenciaSalaoInput = document.getElementById('assistencia-salao-input')
 let assistenciaSalao = 0
+let assistenciaSalaoSalva = 0
 
 
 // data
@@ -9,9 +11,11 @@ const diplayData = document.getElementById('data')
 
 const data = new Date(); // data atual
 const dataFormatada = data.toLocaleString('pt-BR'); // data no formato brasileiro
-console.log(dataFormatada); // exibe a data no console
+
 
 diplayData.innerText = dataFormatada
+
+// assistencia salão
 
 function mostrarAssistencia() {
     assistenciaSalaoInput.innerText = assistenciaSalao
@@ -21,15 +25,21 @@ mostrarAssistencia()
 
 function diminuir() {
     assistenciaSalao = assistenciaSalao - 1
-    console.log(assistenciaSalao)
     mostrarAssistencia()
 }
 
 function somar() {
     assistenciaSalao = assistenciaSalao + 1
-    console.log(assistenciaSalao)
     assistenciaSalaoInput.innerText = assistenciaSalao
+}
+
+function salvarAssistenciaSalao() {
+    assistenciaSalaoSalva = assistenciaSalao
+    console.log(assistenciaSalaoSalva)
 }
 
 botaoMenos.addEventListener('click', diminuir)
 botaoMais.addEventListener('click', somar)
+botaoConcluir.addEventListener('click', salvarAssistenciaSalao)
+
+// 
